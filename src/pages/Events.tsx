4d5afa28eb1ddaed74dd3d8fdbc2774d471625a0
samuelkaranja <samuelkaranja007@gmail.com>
2026-01-04 09:57:50 +0300

    Increased margin top in the Events page

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -4,7 +4,7 @@ const Events: React.FC = () => {
   return (
     <>
       {/* Page Header */}
-      <section className="w-full flex justify-center px-4 sm:px-6 lg:px-8 mt-5">
+      <section className="w-full flex justify-center px-4 sm:px-6 lg:px-8 mt-15">
         <div className="w-full max-w-7xl text-center py-10 lg:py-18">
           <h1 className="text-3xl sm:text-4xl md:text-5xl font-extrabold text-white">
             Happening at Belawinski
