2a48edbf897ffc96a97db2c7bc12e33c6418e495
samuelkaranja <samuelkaranja007@gmail.com>
2026-01-23 21:10:01 +0300

    Added a logic to fetch images from Supabase in the Gallery page

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -1,69 +1,19 @@
+import { useEffect, useState } from "react";
 import { GalleryImage } from "../components";
-import Crowd from "../assets/Belawinski/crowd.jpg";
-import Mix from "../assets/Belawinski/mixologist.jpg";
-import Seat from "../assets/Belawinski/seat1.jpeg";
-import Event from "../assets/Belawinski/halloween1.jpeg";
-import Event2 from "../assets/Belawinski/halloween2.jpeg";
-import Event3 from "../assets/Belawinski/halloween3.jpeg";
-import Game from "../assets/Belawinski/game1.jpeg";
-import Game1 from "../assets/Belawinski/game2.jpeg";
-import Game2 from "../assets/Belawinski/game3.jpeg";
-import Game3 from "../assets/Belawinski/game4.jpeg";
-import Game4 from "../assets/Belawinski/game5.jpeg";
-import Cocktail from "../assets/Belawinski/cocktail6.jpg";
-
-const galleryImages = [
-  {
-    id: 1,
-    src: Crowd,
-  },
-  {
-    id: 2,
-    src: Mix,
-  },
-  {
-    id: 3,
-    src: Seat,
-  },
-  {
-    id: 4,
-    src: Event,
-  },
-  {
-    id: 5,
-    src: Event2,
-  },
-  {
-    id: 6,
-    src: Event3,
-  },
-  {
-    id: 7,
-    src: Game,
-  },
-  {
-    id: 8,
-    src: Game1,
-  },
-  {
-    id: 9,
-    src: Game2,
-  },
-  {
-    id: 10,
-    src: Game3,
-  },
-  {
-    id: 11,
-    src: Game4,
-  },
-  {
-    id: 12,
-    src: Cocktail,
-  },
-];
+import { fetchGalleryImages } from "../services/supabase";
 
 const Gallery: React.FC = () => {
+  const [gallery, setGallery] = useState<any[]>([]);
+
+  useEffect(() => {
+    const loadImageData = async () => {
+      const galleryData = await fetchGalleryImages();
+      setGallery(galleryData);
+    };
+
+    loadImageData();
+  }, []);
+
   return (
     <section className="w-full flex justify-center px-4 sm:px-6 lg:px-8 py-24">
       <div className="w-full max-w-7xl text-center">
@@ -80,11 +30,11 @@ const Gallery: React.FC = () => {
 
         {/* Image Grid */}
         <div className="mt-16 grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 gap-4">
-          {galleryImages.map((image) => (
+          {gallery.map((image) => (
             <GalleryImage
               key={image.id}
-              src={image.src}
-              alt="Belawinski Bar gallery"
+              src={image.image_url}
+              alt={image.title}
             />
           ))}
         </div>
